4b77edeecf6d9666c2855b65ca871fa02619a442
Artsiom Martynau <xzfantom@gmail.com>
2022-04-22 17:16:34 +0400

    task 3

---
+++ 3_ConditionalStatementsAndDataTypes.js
@@ -0,0 +1,22 @@
+var str = "Test string"
+var bool = true
+var num = 10
+
+console.log(`String + boolean = ${str + bool}`)
+console.log(`String + number  = ${str + num}`)
+console.log(`Number + boolean = ${num + bool}`)
+
+console.log(`String * boolean = ${str * bool}`)
+console.log(`String * number  = ${str * num}`)
+console.log(`Number * boolean = ${num * bool}`)
+
+console.log(`String / boolean = ${str / bool}`)
+console.log(`String / number  = ${str / num}`)
+console.log(`Number / boolean = ${num / bool}`)
+
+console.log(`String(boolean)  = ${String(bool)}`)
+console.log(`String(number)   = ${String(num)}`)
+console.log(`Boolean(string)  = ${Boolean(str)}`)
+console.log(`Boolean(number)  = ${Boolean(num)}`)
+console.log(`Number(string)   = ${Number(str)}`)
+console.log(`Number(boolean)  = ${Number(bool)}`)
